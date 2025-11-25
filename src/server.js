814e108c927294ca36514e6f391f7a519279affc
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2025-11-25 21:35:41 +0100

    initial commit for 02-mongodb

---
+++ src/server.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// Маршрути
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!' });
+});
+
+// Middleware для 404
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
+// підключення до MongoDB
+await connectMongoDB();
+
+// Старт сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
