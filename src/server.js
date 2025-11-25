b71957134b2b39124d4cf7d786ab042f322cc58a
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2025-11-25 22:24:51 +0100

    Setup MongoDB, Note model, routes, and middleware fixes for Render

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRouter from './routes/notesRoutes.js';
-import { logger } from './middleware/logger.js';
+import logger from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
