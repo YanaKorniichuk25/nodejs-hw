017468a4a88fcd7fc054940ddd78cd92b04d6ffb
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2025-12-11 10:18:51 +0100

    Fix import

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import User from '../models/user.js';
-import Session from '../models/session.js';
+import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 
 export const registerUser = async (req, res, next) => {
